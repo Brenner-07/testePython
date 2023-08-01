cc484721b1d6f6917dbde6aa11cb10f213321688
Brenner <email>
2023-08-01 20:57:12 -0300

    Terceira alteração

--- testePython.py
+++ testePython.py
@@ -1,4 +1,4 @@
-idade = str(input("Qual sua idade? "))
+idade = str(input("Me diga... Qual sua idade? "))
 print(f"Voce tem: " + str(idade) + " anos.")
 print("Muito obrigado!")
 print("Urruull consegui, eu acho! o.O")
